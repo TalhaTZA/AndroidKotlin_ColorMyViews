fd5cf25fcb58f5102d0741587798ee8923660865
TalhaTZA <talhavjtza@gmail.com>
2018-11-12 15:23:14 +0500

    ADD buttons at bottom and ADD click listeners to those as well

--- app/src/main/java/kt/enterprises/com/colormyviews/MainActivity.kt
+++ app/src/main/java/kt/enterprises/com/colormyviews/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,7 +19,8 @@ class MainActivity : AppCompatActivity() {
     private fun setListeners() {
         val clickableViews: List<View> =
             listOf(box_one_text, box_two_text, box_three_text,
-                box_four_text, box_five_text,constraint_layout)
+                box_four_text, box_five_text, constraint_layout,
+                red_button, green_button, yellow_button)
 
         for (item in clickableViews) {
             item.setOnClickListener { makeColored(it) }
@@ -36,6 +38,12 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
             R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
